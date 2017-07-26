9d9c97138022c43106c00ec81640e8f555d5ca32
Abdullah Al Zubair <0101.jahid@gmail.com>
2017-07-26 20:25:29 +0600

    add all files

---
+++ js/menu.js
@@ -0,0 +1,35 @@
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+
+    $('.submenuItems').hide();
+  });
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+
+  //toggle the sub menu
+  $('.portfolio').click(function(){
+    $('.submenuItems').toggle('slow');
+
+    $('.fa-plus').toggleClass('fa-minus');
+  });
+
+};
+
+$(document).ready(main);
